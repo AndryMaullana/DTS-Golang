7de7b8bd09d5ec8ee1798dffc5e4bd310df68c3c
AndryMaullana <if20.andrimaulana@mhs.ubpkarawang.ac.id>
2023-03-08 16:11:50 +0700

    menambahkan switch fallthrough

--- src/switch.go
+++ src/switch.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	var number = 7
@@ -15,7 +17,7 @@ func main() {
 		fmt.Println("not bad")
 	}
 
-	// Switch dengan perbandingan
+	// Switch with relational operators
 	var score int32 = 6
 	switch {
 	case score == 6:
@@ -28,6 +30,45 @@ func main() {
 		{
 			fmt.Println("Study Harder")
 			fmt.Println("You don't have a good score yet")
+		}
+	}
+	// Switch ( Fallthrough Keyword)
+	fmt.Println("=====================")
+	switch {
+	case score == 6:
+		fmt.Println("perfect")
+
+	case (score < 8) && (score > 3):
+		fmt.Println("not bad")
+		fallthrough // kata kunci ini akan melanjutkan kode di bawah walaupun kondisinya bernilai true
+	case score < 5:
+		fmt.Println("It is oke, but study harder")
+	default:
+		{
+			fmt.Println("Study Harder")
+			fmt.Println("You don't have a good score yet")
+		}
+	}
+	// Switch dan if digabung menjadi kondisi bersarang
+	fmt.Println("=================================")
+	var score1 int = 0
+	if score1 > 7 {
+		switch score1 {
+		case 10:
+			fmt.Println("perfect")
+		default:
+			fmt.Println("nice!")
+		}
+	} else {
+		if score1 == 5 {
+			fmt.Println("Not Bad")
+		} else if score1 == 3 {
+			fmt.Println("Keep Trying")
+		} else {
+			fmt.Println("You can do it")
+			if score1 == 0 {
+				fmt.Println("try harder")
+			}
 		}
 	}
 }
